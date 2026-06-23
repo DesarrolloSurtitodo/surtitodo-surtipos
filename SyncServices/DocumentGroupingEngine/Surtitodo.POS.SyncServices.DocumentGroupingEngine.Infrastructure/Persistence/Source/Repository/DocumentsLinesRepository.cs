d612f9d91600417421b0062ee48a3168e6a67baa
jag062002 <juanchogallego2010@hotmail.com>
2026-06-23 15:11:43 -0500

    feat: crear tabla DEFAULT_CUSTOMERS en la base de datos de origen

---
+++ SyncServices/DocumentGroupingEngine/Surtitodo.POS.SyncServices.DocumentGroupingEngine.Infrastructure/Persistence/Source/Repository/DocumentsLinesRepository.cs
@@ -0,0 +1,14 @@
+﻿using Surtitodo.POS.SyncServices.DocumentGroupingEngine.Application.Interfaces.Persistence.Source;
+using Surtitodo.POS.SyncServices.DocumentGroupingEngine.Domain.Source;
+
+namespace Surtitodo.POS.SyncServices.DocumentGroupingEngine.Infrastructure.Persistence.Source.Repository
+{
+    public class DocumentsLinesRepository : IDocumentsLinesRepository
+    {
+        public async Task<IEnumerable<DocumentsLines>> GetGroupedLinesAsync(IEnumerable<int> ticodiList, string bocodi, string cacodi, 
+            string tipdoc, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SyncServices/DocumentGroupingEngine/Surtitodo.POS.SyncServices.DocumentGroupingEngine.Infrastructure/Persistence/Source/Repository/DocumentsRepository.cs
@@ -0,0 +1,19 @@
+﻿using Surtitodo.POS.SyncServices.DocumentGroupingEngine.Application.Interfaces.Persistence.Source;
+using Surtitodo.POS.SyncServices.DocumentGroupingEngine.Domain.Source;
+
+namespace Surtitodo.POS.SyncServices.DocumentGroupingEngine.Infrastructure.Persistence.Source.Repository
+{
+    public class DocumentsRepository : IDocumentsRepository
+    {
+        public async Task<IEnumerable<Documents>> GetCandidatesAsync(CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateGroupStatusAsync(IEnumerable<int> ticodiList, string bocodi, string cacodi, string tipdoc, string statusCode, long? groupedDocumentId, 
+            string? message, string? logFile, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
