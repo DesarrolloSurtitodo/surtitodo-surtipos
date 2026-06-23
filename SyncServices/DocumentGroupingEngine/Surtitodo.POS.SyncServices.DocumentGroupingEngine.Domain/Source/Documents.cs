3714fdc9e99a8db6c8a3e981fb1c485069548a05
jag062002 <juanchogallego2010@hotmail.com>
2026-06-23 12:13:11 -0500

    feat: crear dominio de entidades DOCUMENTS (source)

---
+++ SyncServices/DocumentGroupingEngine/Surtitodo.POS.SyncServices.DocumentGroupingEngine.Domain/Source/Documents.cs
@@ -0,0 +1,13 @@
+﻿namespace Surtitodo.POS.SyncServices.DocumentGroupingEngine.Domain.Source
+{
+    public class Documents
+    {
+        public required string BOCODI {  get; set; } 
+        public required string CACODI {  get; set; }
+        public required string TIPDOC { get; set; }
+        public int TICODI { get; set; }
+        public DateTime TIDATA { get; set; }
+        public required string CLCODI { get; set; }
+        public decimal TITOT { get; set; }
+    }
+}
---
+++ SyncServices/DocumentGroupingEngine/Surtitodo.POS.SyncServices.DocumentGroupingEngine.Domain/Source/DocumentsLines.cs
@@ -0,0 +1,14 @@
+﻿namespace Surtitodo.POS.SyncServices.DocumentGroupingEngine.Domain.Source
+{
+    public class DocumentsLines
+    {
+        public required string BOCODI { get; set; }
+        public required string CACODI { get; set; }
+        public required string TIPDOC { get; set; }
+        public required string ARCODI { get; set; }
+        public int TLQTT { get; set; }
+        public decimal TLTOT { get; set; }
+        public int TLPDTE { get; set; }
+        public required string CODIGO_IVA { get; set; }
+    }
+}
