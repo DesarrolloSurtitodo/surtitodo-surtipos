96c946d5bca572d70da409c5ae0c38cb07cbf62d
jag062002 <juanchogallego2010@hotmail.com>
2026-06-24 08:02:14 -0500

    feat: se implementa la interfaz IDocumentAgroupTraceRepository

---
+++ SyncServices/DocumentGroupingEngine/Surtitodo.POS.SyncServices.DocumentGroupingEngine.Infrastructure/Persistence/Target/Repository/DocumentAgroupTraceRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Surtitodo.POS.SyncServices.DocumentGroupingEngine.Application.Interfaces.Persistence.Target;
+using Surtitodo.POS.SyncServices.DocumentGroupingEngine.Domain.Target;
+
+namespace Surtitodo.POS.SyncServices.DocumentGroupingEngine.Infrastructure.Persistence.Target.Repository
+{
+    public class DocumentAgroupTraceRepository(AppDbContext ctx) : IDocumentAgroupTraceRepository
+    {
+        private readonly AppDbContext _ctx = ctx;
+
+        public async Task<long?> FindAgroupIdAsync(string bocodi, string cacodi, string tipdoc, int ticodi, CancellationToken ct = default)
+        {
+            var trace = await _ctx.DocumentAgroupTrace
+                .AsNoTracking()
+                .FirstOrDefaultAsync(t =>
+                    t.BOCODI == bocodi &&
+                    t.CACODI == cacodi &&
+                    t.TIPDOC == tipdoc &&
+                    t.TICODI == ticodi, ct);
+
+            return trace?.DocumentAgroupId;
+        }
+
+        public async Task InsertManyAsync(IEnumerable<DocumentAgroupTrace> traces, CancellationToken ct = default)
+        {
+            await _ctx.DocumentAgroupTrace.AddRangeAsync(traces, ct);
+        }
+    }
+}
